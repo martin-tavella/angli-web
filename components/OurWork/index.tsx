6f16097c0e9197404a1b2df6c65d6cdf472bda4d
martin-tavella <martutavella@gmail.com>
2025-11-11 20:59:03 -0300

    ourWork title more responsive

--- components/OurWork/index.tsx
+++ components/OurWork/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Ticker from "@/utils/ticker";
 import GridWorkCarouselEmbla from "./GridWorkCarousel/gridWorkCarousel";
 import titulo from "@/public/ourWork/TITULO.png";
@@ -8,7 +9,7 @@ const OurWork = () => {
     return (
         <section className="relative">
             <Ticker color="red" />
-            <img src={titulo.src} alt="Nuestros trabajos" className="absolute -top-10 left-[41%]  w-[20%] z-70 title"/>
+            <img src={titulo.src} alt="Nuestros trabajos" className="absolute sm:-top-10 md:w-[42%] md:left-[31%] lg:w-[30%] lg:left-[36%]       -top-5 left-[27%] w-[50%] z-40 title"/>
             <GridWorkCarouselEmbla />
             <Ticker color="red" />
         </section>
